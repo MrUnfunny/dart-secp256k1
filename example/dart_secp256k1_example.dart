b36d44613ce7a9af1ef2a6901d3425bfb0020629
Mohit <mrunfunny2599@gmail.com>
2022-11-20 13:35:36 +0530

    refactor: basic refactoring

--- example/dart_secp256k1_example.dart
+++ example/dart_secp256k1_example.dart
@@ -1,6 +1,81 @@
-// import 'package:dart_secp256k1/dart_secp256k1.dart';
+import 'package:dart_secp256k1/dart_secp256k1.dart';
+import 'package:dart_secp256k1/src/classes.dart';
 
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+void main() {
+  var seckey1 = [
+    0xd7,
+    0xfe,
+    0x9b,
+    0x49,
+    0xd0,
+    0x63,
+    0x1e,
+    0x36,
+    0x82,
+    0x8c,
+    0xbc,
+    0xc7,
+    0xf4,
+    0xaf,
+    0x8b,
+    0x29,
+    0x3e,
+    0x44,
+    0x3c,
+    0xa8,
+    0x40,
+    0xdf,
+    0xb8,
+    0x9a,
+    0xef,
+    0x4f,
+    0x4a,
+    0xb0,
+    0x5e,
+    0x1f,
+    0xb3,
+    0x07,
+  ];
+  var seckey2 = [
+    0xff,
+    0xba,
+    0x10,
+    0xfb,
+    0x25,
+    0x4d,
+    0xb0,
+    0x04,
+    0xc7,
+    0x60,
+    0xfe,
+    0x24,
+    0x40,
+    0x20,
+    0x12,
+    0xc6,
+    0xae,
+    0xc3,
+    0xed,
+    0x2e,
+    0x7e,
+    0xfb,
+    0x7d,
+    0x77,
+    0x9a,
+    0x53,
+    0x3a,
+    0x44,
+    0x92,
+    0xda,
+    0xe1,
+    0x8b,
+  ];
+  var pubKeyHex =
+      "043b5ac2b005c78297272c0f5dbeefd88cec42db09392ac7cb1e2c64689ca1fe634631916ee95dbd892ffeda37e31d04689aa1715fa1c7dc6f8a5fcdf20c3ffa78";
+
+  final secret = Secp256k1().ecdh(
+    seckey1,
+    Secp256k1PublicKey.fromHex(pubKeyHex),
+  );
+  print('secret is $secret');
+}
